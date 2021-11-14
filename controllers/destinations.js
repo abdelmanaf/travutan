2138abbebb1a5e3fc2626cca5a766ae354932c91
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-14 17:24:00 -0500

    add update function

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,29 +1,76 @@
-import {Profile} from './../models/profile.js'
-import {Destination} from './../models/destination.js'
+import {
+    Profile
+} from './../models/profile.js'
+import {
+    Destination
+} from './../models/destination.js'
 
-function create(req, res){
+function create(req, res) {
     console.log(req.body)
     Destination.create(req.body)
-    .then(result => res.json(result))
-    .catch(err => console.log(err))
+        .then(result => res.json(result))
+        .catch(err => console.log(err))
 }
 
-function index(req, res){
+function index(req, res) {
     Destination.find({})
-    .then(destinations => {
-        res.status(200).json(destinations)
-    })
-    .catch(err => {
-    res.json(err)
-    })
+        .then(destinations => {
+            res.status(200).json(destinations)
+        })
+        .catch(err => {
+            res.json(err)
+        })
 }
 
-function deleteDestination(req, res){
-    console.log()
+function show(req, res) {
+    const id = req.params.id
+    Destination.findOne(id)
+        .then(destination => {
+            res.status(200).json(destination)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+}
+
+function deleteDestination(req, res) {
+    const id = req.params.id;
+    Destination.findOneAndDelete(id).then(destination => {
+            res.status(200).json(destination)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+
+}
+
+function update(req, res) {
+    const id = req.params.id;
+
+
+    Destination.findOneAndUpdate(id, req.body, {
+            upsert: true
+        }).then(destination => {
+            res.status(200).json(destination)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+
+
+
+    // Destination.findOneAndUpdate(id, req.body, {upsert: true}, function(err, dest) {
+    //     if (err) return res.send(500, {error: err});
+    //     return res.status(200).json(dest);
+    // });
+
+
 }
 
 export {
     create,
     deleteDestination as delete,
-    index
+    index,
+    update,
+    show
 }
--- routes/destinations.js
+++ routes/destinations.js
@@ -4,8 +4,10 @@ import * as destinationsCtrl from './../controllers/destinations.js'
 const router = Router()
 
 router.get('/', destinationsCtrl.index)
+router.get('/:id', destinationsCtrl.show)
 router.post('/', destinationsCtrl.create)
 router.delete('/:id', destinationsCtrl.delete)
+router.put('/:id', destinationsCtrl.update)
 
 export {
   router
